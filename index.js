b42d50bb48dc50d8c6dd112cbcb3f0a58d288342
Manny manning <60320088+Abdurahmanm2020@users.noreply.github.com>
2020-04-18 21:38:47 +0200

    Add files via upload
    this is a calculator that I made ( https://github.com/Abdurahmanm2020/Calculator.git )

---
+++ index.js
@@ -0,0 +1,18 @@
+ function dis(val) 
+         { 
+             document.getElementById("result").value+=val 
+         } 
+           
+         //function that evaluates the digit and return result 
+         function solve() 
+         { 
+           let x = document.getElementById("result").value 
+             let y = eval(x) 
+             document.getElementById("result").value = y 
+         } 
+           
+         //function that clear the display 
+         function clr() 
+         { 
+             document.getElementById("result").value = "" 
+         } 
